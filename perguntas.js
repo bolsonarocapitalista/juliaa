e43a456d08e692d9eb5d8b92420bac1103f9702d
tamiiilyly <161335688+tamiiilyly@users.noreply.github.com>
2024-10-18 09:12:57 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -3,91 +3,115 @@ criaCartao(
     'O que é fotossíntese?',
     'Quais são os principais grupos de seres vivos?'
 )
+
 criaCartao(
     'Matemática',
     'O que é um número primo?',
     'Qual é a fórmula da área de um triângulo?'
 )
+
 criaCartao(
     'Portugues',
     'O que é uma oração subordinada?',
     'Qual é a diferença entre substantivo e adjetivo?'
 )
+
 criaCartao(
     'Matemática',
     'Qual a ordem de prioridade em expressão numérica?',
     'Parênteses, colchetes e chaves'
 )
+
 criaCartao(
     'Matemática',
     'Qual a ordem de prioridade em expressão numérica?',
     'Parênteses, colchetes e chaves'
 )
+
 criaCartao(
     'Matemática',
     'Qual a ordem de prioridade em expressão numérica?',
     'Parênteses, colchetes e chaves'
 )
+
 criaCartao(
     'Matemática',
     'Qual a ordem de prioridade em expressão numérica?',
     'Parênteses, colchetes e chaves'
 )
+
 criaCartao(
     'Matemática',
     'Qual a ordem de prioridade em expressão numérica?',
     'Parênteses, colchetes e chaves'
 )
+
 criaCartao(
     'Matemática',
     'Qual a ordem de prioridade em expressão numérica?',
     'Parênteses, colchetes e chaves'
 )
+
 criaCartao(
     'Matemática',
     'Qual a ordem de prioridade em expressão numérica?',
     'Parênteses, colchetes e chaves'
 )
+
 criaCartao(
     'Matemática',
     'Qual a ordem de prioridade em expressão numérica?',
     'Parênteses, colchetes e chaves'
 )
+
 criaCartao(
     'Matemática',
     'Qual a ordem de prioridade em expressão numérica?',
     'Parênteses, colchetes e chaves'
 )
+
 criaCartao(
     'Matemática',
     'Qual a ordem de prioridade em expressão numérica?',
     'Parênteses, colchetes e chaves'
 )
+
 criaCartao(
     'Matemática',
     'Qual a ordem de prioridade em expressão numérica?',
     'Parênteses, colchetes e chaves'
 )
+
 criaCartao(
     'Matemática',
     'Qual a ordem de prioridade em expressão numérica?',
     'Parênteses, colchetes e chaves'
 )
+
 criaCartao(
     'Matemática',
     'Qual a ordem de prioridade em expressão numérica?',
     'Parênteses, colchetes e chaves'
 )
+
 criaCartao(
     'Matemática',
     'Qual a ordem de prioridade em expressão numérica?',
     'Parênteses, colchetes e chaves'
 )
+
 criaCartao(
     'Matemática',
     'Qual a ordem de prioridade em expressão numérica?',
     'Parênteses, colchetes e chaves'
 )
+
+criaCartao(
+    'Matemática',
+    'Qual a ordem de prioridade em expressão numérica?',
+    'Parênteses, colchetes e chaves'
+)
+
 criaCartao(
     'Matemática',
     'Qual a ordem de prioridade em expressão numérica?',
