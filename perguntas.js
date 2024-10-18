6f48eccb0965a5421d1aff893a108c7b35bff91a
tamiiilyly <161335688+tamiiilyly@users.noreply.github.com>
2024-10-18 08:29:23 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,7 @@
+criaCartao(
+    'Matemática',
+    'Qual a ordem de prioridade em expressão numérica?',
+    'Parênteses, colchetes e chaves'
+)
+
+*Repetir isso 20 vezes e com perguntas de diferentes matérias 
