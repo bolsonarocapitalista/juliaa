9bae02e909b91f27b87497b934595c08d9c82f8d
tamiiilyly <161335688+tamiiilyly@users.noreply.github.com>
2024-10-18 09:14:46 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -117,3 +117,33 @@ criaCartao(
     'Qual a ordem de prioridade em expressão numérica?',
     'Parênteses, colchetes e chaves'
 )
+
+criaCartao(
+    'Matemática',
+    'Qual a ordem de prioridade em expressão numérica?',
+    'Parênteses, colchetes e chaves'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a ordem de prioridade em expressão numérica?',
+    'Parênteses, colchetes e chaves'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a ordem de prioridade em expressão numérica?',
+    'Parênteses, colchetes e chaves'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a ordem de prioridade em expressão numérica?',
+    'Parênteses, colchetes e chaves'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a ordem de prioridade em expressão numérica?',
+    'Parênteses, colchetes e chaves'
+)
