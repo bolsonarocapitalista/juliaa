85a9b07a8fbd4c3eae0bfbb64542e0d8ce8eb7c5
bolsonarocapitalista <142528137+bolsonarocapitalista@users.noreply.github.com>
2024-11-04 08:11:42 -0300

    ............

--- pagina 2.js
+++ pagina 2.js
@@ -3,7 +3,7 @@ function criaCartao (categoria, pergunta, resposta){
     let cartao = document.createElement('article');
     cartao.className = 'cartao'
     cartao.innerHTML = `
-    <div class="conteudo-cartao" onclick="mostrarResposta(this)">
+    <div class="conteudo-cartao" onclick= mostrarResposta(this)>
     <h3>${categoria}</h3>
     <div class="pergunta-cartao">
         <p>${pergunta}</p>
@@ -21,7 +21,6 @@ function mostrarResposta(card){
     const resposta = card.querySelector('.resposta-cartao');
     if(resposta.style.display === 'none'){
         resposta.style.display = 'block';
-
     }
     else{
         resposta.style.display = 'none';
